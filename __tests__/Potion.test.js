3b50ed4efedbbc7bcf17f3ffca680adea522903d
EsteMaeLeo <leonel_morales20@hotmail.com>
2022-02-16 11:09:38 -0600

    player object

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -2,7 +2,7 @@ const Potion = require('../lib/Potion.js');
 
 test('creates a random potion object', () => {
     const potion = new Potion();
-  
+  console.log(potion);
     expect(potion.name).toEqual(expect.any(String));
     expect(potion.name.length).toBeGreaterThan(0);
     expect(potion.value).toEqual(expect.any(Number));
